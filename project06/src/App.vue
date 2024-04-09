370a17dee023f3c065752e6f3aba2158e2353650
honghuiseung <huiseung1082@daum.net>
2024-04-09 12:34:20 +0900

    modal

---
+++ project06/src/App.vue
@@ -0,0 +1,61 @@
+<!-- WishInput.vue -->
+<!-- 1번째 문제 : WishInput입력했을 때 f5를 눌러야 반영-->
+<!-- 2번째 문제 : WishFooter전부삭제할 때 f5를 눌러야 반영-->
+<!-- 3번째 문제 : App.vue에서 wishItems라는 데이터를 정의하고 하위 WishList에 props로 전달-->
+<template lang="html">
+    <div id="app">
+        <WishHeader></WishHeader>
+        <!-- 앞에 addWish는 이벤트(하위에서 함수 호출하는 이름), 뒤의 addWish는 App.vue에 정의한 함수 -->
+        <!-- v-on은 @로 대체할수 있다. -->
+        <WishInput v-on:addWish="addWish"></WishInput>
+        <WishList v-bind:propsData="wishItems" @removeWish="removeWish"></WishList>
+        <WishFooter v-on:removeAll="clearAll"></WishFooter>
+    </div>
+</template>
+<script>
+    import WishHeader from './components/WishHeader.vue'
+    import WishInput from './components/WishInput.vue'
+    import WishList from './components/WishList.vue'
+    import WishFooter from './components/WishFooter.vue'
+    export default{
+        data(){
+            return{
+                wishItems: []
+            }
+        },
+        created(){
+            if(localStorage.length > 0){
+                for(var i = 0; i <localStorage.length; i++){
+                    this.wishItems.push(localStorage.key(i));
+                }
+            }
+        },
+        methods:{
+            addWish(wishItem){
+                //로컬 스토리지에 데이터를 추가하는 로직
+                localStorage.setItem(wishItem,wishItem);
+                this.wishItems.push(wishItem);
+            },
+            clearAll(){
+                localStorage.clear();
+                this.wishItems = [];
+            },
+            removeWish(wishItem, index){
+                localStorage.removeItem(wishItem);
+                this.wishItems.splice(index, 1);
+            }
+        },
+        components:{
+            'WishHeader': WishHeader,
+            'WishInput': WishInput,
+            'WishList': WishList,
+            'WishFooter': WishFooter
+        }
+    }
+</script>
+<style lang="css">
+    body{text-align: center; background-color: #f6f6f8;}
+    input{border-style: groove; width: 200px;}
+    button{border-style: groove;}
+    .shadow{box-shadow: 5px 10px 10px rgba(0,0,0,0.03);}
+</style>
---
+++ project06/src/components/WishList.vue
@@ -0,0 +1,70 @@
+<!-- WishList.vue -->
+<template lang="html">
+<section>
+  <!--선택한 위시리스트를 뷰에서 인식하도록 만들기 할 차례-->
+  <!--npm run dev-->
+  <transition-group name="list" tag="ul">
+    <li v-for="(wishItem, index) in propsData" v-bind:key="wishItem" class="shadow">
+      <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+      {{wishItem}}
+      <span class="removeBtn" type="button" @click="removeWish(wishItem, index)">
+        <i class="far fa-trash-alt" aria-hidden="true"></i>
+      </span>
+    </li>
+  </transition-group>
+</section>
+</template>
+<script>
+  export default{
+    props: ['propsData'],
+    //data(){
+    //  return{
+    //    wishItems:[]
+    //  }
+    //},
+    methods:{
+      removeWish(wishItem, index){
+        //console.log(wishItem, index);
+        //localStorage.removeItem(wishItem);
+        //this.wishItems.splice(index, 1);
+        this.$emit('removeWish', wishItem, index);
+      }
+    }
+  }
+</script>
+<style lang="css" scoped>
+.list-enter-active, .list-leave-active{
+  transition: all 1s;
+}
+.list-enter, .list-leave-to{
+  opacity:0;
+  transform: translateY(30px);
+}
+  ul{
+    list-style-type: none;
+    padding-left: 8px;
+    margin-top: 0px;
+    text-align: left;
+  }
+  li{
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5em 0;
+    padding: 0 0.9em;
+    background: #fefefe;
+    border-radius: 5px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .checkBtn{
+    line-height: 50px;
+    color: #ff8000;
+    margin-right: 5px;
+  }
+  .removeBtn{
+    margin-left: auto;
+    color: #de4343;
+  }
+</style>
